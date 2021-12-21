449460d8b059890c1fa4858c2fe2630364863bba
Jeral <jeral.cris@gmail.com>
2021-12-20 22:30:14 -0600

    Agregando styled components a la app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+   display: flex;
+   align-items: center;
+   flex-direction: column-reverse;
+   padding-top: 5rem;
+`;
+
+const Boton = styled.button`
+   background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+   background-size: 300px;
+   border: 2px solid #000;
+   font-family: Arial, Helvetica, sans-serif;
+   color: #fff;
+   cursor: pointer;
+   font-size: 2rem;
+   margin-top: 3rem;
+   padding: 1rem;
+`;
+
 function App() {
+   const consultarApi = () => {
+      console.log('consultando');
+   };
    return (
-      <>
-         <h1>Breaking Bad</h1>
-      </>
+      <Contenedor>
+         <Boton onClick={consultarApi}>Obtener Frase</Boton>
+      </Contenedor>
    );
 }
 
